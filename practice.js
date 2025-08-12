60da0f1cf17785ec36682e72f30497c1647b1309
danishkhan8909 <khandanish909577@gmail.com>
2025-08-12 16:59:23 +0530

    first commit

---
+++ practice.js
@@ -0,0 +1,228 @@
+  // let a=20;
+  // let b=10;
+//   let c=a+b;
+// //   console.log("answer is",c )
+// // document.writeln(c);
+
+// // let a= parseInt (prompt("enter a value for a :"));
+// // let b= parseInt(prompt ("enter a value for b :"));
+// let c= a+b;
+// console.log(c);
+// // alert(c);
+// // document writein ("answer is sum")
+// document.getElementById("device").innerHTML=c
+
+// if(a>b)
+// {
+// console.log("hi everyone...");
+// console.log("Greate value is ",a)
+// }
+// else
+//   console.log("Greater value is",b);
+// if(a<b)
+// {
+// console.log("hi everyone..");
+// console.log("less than is",b)
+// }
+// else
+//   console.log("less value is",a)
+
+// let marks=100;
+// if(marks<=40)
+// console.log("fail");
+// else
+//   if(marks<=60)
+//     console.log("pass")
+//   else
+//     if(marks<=50)
+//       console.log("second class")
+//     else
+//       if(marks<=100)
+//         console.log("first class")
+
+// for(let i=0; i<5; i++)
+// {
+//   console.log("Hello world")
+// }
+
+//  let a=0
+//   while (a<10){
+// console.log(a);
+// a++
+//   }
+
+// let i=0;
+// do
+// {
+// console.log("i is", i);
+// i++;
+// }
+// while(i<5)
+
+// let fruits=["banana", "mango", "apple"]
+// for (let fruit of fruits){
+//   console.log(fruit)
+// }
+
+// let word= "Hey world"
+// for(let char of word){
+//   console.log(char)
+// }
+
+// let student={
+// Name : "Danish",
+// Age: "25",
+// city : "Azamgarh"
+// }
+// for (let key in student){
+//   console.log(key,":",student[key])
+// }
+
+// let st== prompt("enter status:")
+// if (st=="a" || st=="A")
+// document. writeln("1 lac approved");
+// else
+//   if(st=="b"|| st=="B")
+//     document.writeln("75000 approved");
+//   else
+//   if(st=="c" || st=="C")
+//     document.writeln("50000 approvrd..")
+//   else
+//     if(st=="d" || st=="D")
+//       document.writeln("not approved.")
+//     else
+//       document.writeln("invalid status")
+
+// let st= prompt("enter status")
+// switch(st)
+// {
+// case "a" :
+// case "A" :
+//   document.writeln("1 lac approved");
+//   break;
+
+//   case "b":
+//   case "B":
+//   document.writeln("75000 approved");
+//   break;
+
+//   case "c" :
+//   case "C" :
+//     document.writeln("50000 approvrd..")
+//     break;
+
+//     case "d":
+//     case "D":
+//       document.writeln("not approved.")
+//       break;
+//       default:
+//          document.writeln("invalid status")
+// }
+
+// for (let i=0; i<5; i++)
+// {
+//  console.log ("hey india")
+// }
+
+// for(let c=5;c<=50;c++)
+// {
+//   console.log(c);
+// }
+
+// let t=5;
+// for(c=1; c<=10; c++)
+// {
+//   console.log(c*t);
+// }
+
+// let t=1
+//  for(let a=1;a<5; a++)
+//  {
+//   t=t+a;
+// console.log(t)
+//  }
+
+// for(let c=1; c<=5; c++){
+
+// console.log("Danish" ,c);
+// }
+// console.log("out")
+
+// const arr =[10,20,30,40,50,]
+// console.log(arr);
+
+
+// let frits=["Apple","Banana","Grapes"]
+// for(let fruits of fruits){
+//   console.log(fruit)
+// }
+
+// let age=[10,20,30,40,];
+// console.log(age);
+
+// const a=[10,20,30,40,50];
+// console.log(a);
+// for(let c=0;c<=a.length;c++){
+//   console.log(a[c],a.length);
+// }
+
+// let fruit=["mango","apple","grapes"]
+// console.log(fruit);
+// fruit.pop("mango");
+
+// let fruit=["mango","banana","apple"];
+// console.log(fruit);
+// fruit.unshift("mango");
+
+// const state=["Maharastra","Uttarpradesh","Azamgarh"]
+// console.log(state);
+// for(let a of state){
+//   console.log(a.toUpperCase());
+// }
+
+
+// const arr5=new Array(100,200,300);
+// console.log(arr5);
+
+// a=5
+// b=a++
+// console.log(a,b);
+
+// const state=["Maharastra", "Assam","Uttarpradesh","Azamgarh"]
+// const a=state.slice(1,4);
+// console.log(a);
+
+// const no=[1,2,3,4];
+// const a=no.slice(1,2);
+// console.log (a[(a).length -1])
+
+// const sum=(a,b)=>{
+//   return a+b;
+// };console.log(sum(5,6));
+
+
+// let st ={
+//   name: "Danish",
+//   age: 21,
+//   ht: 6.2,
+//   subd: true,
+//   grt : function(){
+//     alert("welcome..")
+//   }
+// }
+// st.grt()
+// console.log(st.subd);
+
+// x = 8
+// y = 6
+// let z ='template litrals ${x+y}';
+// console.log(z);
+
+// let a='template litrals ${"hello"}';
+// console.log(a);
+
+// console.log("student name",st.fname,"age", st.age,"height",st.ht);
+// console.log(`student name ${st.fname} age${st.age} height ${st.ht}`);
+
+
+console.log(123);
